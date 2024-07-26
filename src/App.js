c177e3ec58135cb83f4bfdeda0fbe7907497e923
kinsukSingh <kinsuksingh674@gmail.com>
2024-07-26 17:48:25 +0530

    Adding soil moisture sensor data visulisation pie chart

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+"use client"
+import FirebaseConfig from "../src/components/Firebase/FirebaseConfig"
+import {ref, get, child} from "firebase/database"
+import { useEffect, useState } from "react" 
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Home from './components/Home'
@@ -5,14 +9,61 @@ import NavBar from "./components/NavBar";
 import Page from "./components/Page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const db = FirebaseConfig()
+
 const App = () => {
+    let [wifi, setWifi] = useState('')
+    let [soilMois, setSoilMois] = useState('')
+    let [temp, setTemp] = useState('')
+    let [humidity, setHumidity] = useState('')
+    let [rainStatus, setRainStatus] = useState('')
+    let [waterPumpState, setPumpState] = useState('')
+
+    useEffect(() => {
+        setInterval(() => {
+            getData();
+        }, 1000); // Interval: Fetch data every second
+    }, []);
+
+    let getData = () => {
+        const dbref = ref(db)
+        get(child(dbref, "sensors-reading1")).then(snapshot => {
+            if (snapshot.exists()){
+                setWifi(snapshot.val().WiFi)
+                setSoilMois(snapshot.val().SoilMoisture)
+                setTemp(snapshot.val().Temperature)
+                setHumidity(snapshot.val().Humidity)
+                setRainStatus(snapshot.val().RainStaus)
+                setPumpState(snapshot.val().MotorState)
+            }
+        })
+    }
+  const sensorData = {
+    wifi,
+    soilMois,
+    temp,
+    humidity,
+    rainStatus,
+    waterPumpState
+  }
+
+  const data = [
+    {
+      value: soilMois?soilMois:0,
+      soilMoisStatus: "Moisture",
+    },
+    {
+      value: soilMois?100-soilMois:0,
+      soilMoisStatus: "Dry",
+    },
+  ]
   return(
     <BrowserRouter>
       <NavBar/>
       <div className="main-container">
           <Routes>
-              <Route path="/smart-garden-and-agriculture" element={<Home/>} />
-              <Route path="/smart-garden-and-agriculture/data" element={<Page/>} />
+              <Route path="/smart-garden-and-agriculture" element={<Home sensorData={sensorData}/>} />
+              <Route path="/smart-garden-and-agriculture/data" element={<Page sensorDataForVisulise={data}/>} />
           </Routes>
       </div>
     </BrowserRouter>
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,9 +1,41 @@
+import { PieChart, Pie, Legend, Cell, ResponsiveContainer } from "recharts"
 import './index.css'
-const Page = () => {
-    return(
-        <div className='page-section'>
-        <h1>Page Not Found</h1>
+
+
+
+const Page = (props) => {
+    const {sensorDataForVisulise} = props
+    return (
+        <div className="page-section">
+            <div className="moisture-sensor-card">
+            <h1 className="heading">Soil Moisture Sensor Data Visulisation</h1>
+            <ResponsiveContainer width="100%" height={300}>
+            <PieChart>
+                <Pie
+                    cx="50%"
+                    cy="50%"
+                    data={sensorDataForVisulise}
+                    startAngle={0}
+                    endAngle={360}
+                    innerRadius="20%"
+                    outerRadius="70%"
+                    dataKey="value"
+                >
+                    <Cell name="Moisture" fill="rgb(58, 182, 244)" />
+                    <Cell name="Dry" fill="#9B7653" />
+                </Pie>
+                <Legend
+                    iconType="circle"
+                    layout="horizontal"
+                    verticalAlign="bottom"
+                    align="center"
+                />
+            </PieChart>
+            </ResponsiveContainer>
+            </div>
         </div>
+        
     )
 }
+
 export default Page
